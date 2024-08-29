9ae44e5b23f2ef7ea0700dd5358a0779255a776e
Nail Shiyatov <nailshiyatov.04@mail.ru>
2024-08-29 12:50:15 +0300

    Updated launching WebView from index.html.

--- app/src/main/java/ru/andvl/mytonwallet/contest/MainActivity.kt
+++ app/src/main/java/ru/andvl/mytonwallet/contest/MainActivity.kt
@@ -1,22 +1,19 @@
 package ru.andvl.mytonwallet.contest
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.arkivanov.decompose.defaultComponentContext
 import com.arkivanov.decompose.extensions.compose.stack.animation.LocalStackAnimationProvider
 import org.koin.android.ext.android.get
-import ru.andvl.mytonwallet.contest.blockchain.util.WebViewCompose
 import ru.andvl.mytonwallet.contest.root.api.LocalRootNavigation
 import ru.andvl.mytonwallet.contest.root.api.RootDecomposeComponent
 import ru.andvl.mytonwallet.contest.ui.theme.MyTonWalletContestTheme
@@ -35,13 +32,34 @@ class MainActivity : ComponentActivity() {
                     LocalRootNavigation provides root,
                     LocalStackAnimationProvider provides WalletStackAnimationProvider
                 ) {
-                    root.Render(
-                        Modifier.fillMaxSize()
-                            .imePadding()
-                    )
-                    WebViewCompose()
+//                    root.Render(
+//                        Modifier.fillMaxSize()
+//                            .imePadding()
+//                    )
+                    WebViewScreen()
                 }
             }
         }
     }
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebViewScreen() {
+    AndroidView(factory = { context ->
+        WebView(context).apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.allowFileAccess = true
+            settings.allowContentAccess = true
+            settings.allowUniversalAccessFromFileURLs = true
+            settings.allowFileAccessFromFileURLs = true
+
+            loadUrl("file:///android_asset/index.html")
+        }
+    }
+    )
 }
